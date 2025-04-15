6228cf8f0fe6bab544b7521a5ddedd750fd70bbd
Aditya Sharma <aditya.s@intimetec.com>
2025-04-15 22:55:09 +0530

    fix: updated formatting

--- Moodle-Chapter-5/question2/Main.java
+++ Moodle-Chapter-5/question2/Main.java
@@ -2,12 +2,12 @@ public class Main{
     public static void main (String[] args){
         Customer myCustomer = new Customer();
         Wallet wallet = new Wallet();
+        WalletProcessor walletProcessor = new WalletProcessor();
 
         wallet.setTotalMoney(100);
         myCustomer.setWallet(wallet);
         
         Wallet theWallet = myCustomer.getWallet();
-        WalletProcessor walletProcessor = new WalletProcessor();
         float payment = 2;
 
         if (theWallet.getTotalMoney() > payment) {
